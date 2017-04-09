dc3b6e2fd0224881e4c632ea1737ed876bb1c20f
Cara Chew <carachew@gmail.com>
2017-04-10 00:31:25 +0800

    Completed Creating the User, User Signup, User Sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,21 @@
+var User = require('../models/user')
 var express = require('express');
 var router = express.Router();
+
+router.post('/signup', function (req,res) {
+  User.create({
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password
+  }, function (err, createdUser) {
+    if (err) {
+      console.log('An error occured: ' + err)
+      res.redirect('/auth/signup')
+    } else {
+      res.redirect('/')
+    }
+  })
+})
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var session = require('express-session')
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 var app = express();
+
 
 if (process.env.NODE_ENV === "test") {
   mongoose.connect('mongodb://localhost/express-authentication')
@@ -15,6 +17,11 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', function(req, res) {
   res.render('index');
